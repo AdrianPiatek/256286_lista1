20034bbd29f4257b434c20a95980381e48747282
ulpioro <78817508+ulpioro@users.noreply.github.com>
2021-11-03 20:05:49 +0100

    UNDO changes

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import numpy as np
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
+import tensorflow as tf
+
 
 root = tk.Tk()
 label = tk.Label(root)
 image = 0
+hub_model = tensorflow_hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
 
 
 def overlay_image(background, foreground, alpha):
@@ -22,8 +25,8 @@ def overlay_image(background, foreground, alpha):
 def face_swap(file):
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     img = cv2.imread(file)
-    face = cv2.imread('face.png')
-    mask = cv2.imread('mask.png')
+    face = cv2.imread('../256286_lista_1/face.png')
+    mask = cv2.imread('../256286_lista_1/mask.png')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     for (x, y, w, h) in faces:
@@ -35,27 +38,59 @@ def save_file():
     filename = filedialog.asksaveasfile(mode='w', defaultextension=".png")
     if not filename:
         return
-    cv2.imwrite(filename.name, image)
+    image.save(filename.name)
 
 
-def get_file_name():
+def face_overlay():
     filename = filedialog.askopenfilename()
     global image
     image = face_swap(filename)
-    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    img = ImageTk.PhotoImage(image=Image.fromarray(img))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = Image.fromarray(image)
+    img = ImageTk.PhotoImage(image=image)
 
     label.config(image=img)
     label.image = img
     label.grid(row=0, column=0)
 
     save_button = tk.Button(root, text='save', command=save_file)
-    save_button.grid(row=2, column=0)
+    save_button.grid(row=3, column=0)
+
+
+def tensor_to_image(tensor):
+    tensor = tensor*255
+    tensor = np.array(tensor, dtype=np.uint8)
+    if np.ndim(tensor) > 3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+    return PIL.Image.fromarray(tensor)
+
+
+def change(file_img, file_style):
+    img = tf.io.read_file(file_img)
+    img = tf.image.decode_image(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    style = tf.io.read_file(file_style)
+    style = tf.image.decode_image(style, channels=3)
+    style = tf.image.convert_image_dtype(style, tf.float32)
+
+    stylized_image = hub_model(tf.constant(img), tf.constant(style))[0]
+    return tensor_to_image(stylized_image)
+
+
+def change_style():
+    filename_img = filedialog.askopenfilename()
+    filename_style = filedialog.askopenfilename()
+    global image
+    image = change(filename_img, filename_style)
 
 
 def gui():
-    button = tk.Button(root, text='wybierz plik', command=get_file_name)
-    button.grid(row=1, column=0)
+    button_face = tk.Button(root, text='face overlay', command=face_overlay)
+    button_face.grid(row=1, column=0)
+    button_style = tk.Button(root, text='change style', command=change_style)
+    button_style.grid(row=2, column=0)
     root.mainloop()
 
 
